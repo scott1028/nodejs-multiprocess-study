4f19329ac856f21aec84a9434d057a01ae729c32
scott.lan <scott.lan@taisys.com>
2017-08-31 14:04:30 +0800

    update code

--- sample02.js
+++ sample02.js
@@ -9,12 +9,13 @@ if(cluster.isMaster) {
     console.log('I am master');
     // Pass parameter to worker.
     // Ref: https://stackoverflow.com/questions/12495231/node-js-fork-function-and-passing-arguments-to-child-process
-    cluster.fork({wid: `${1}`});
-    cluster.fork({wid: `${2}`});
+    cluster.fork({preload: JSON.stringify({ b: 1 })});
+    cluster.fork({preload: JSON.stringify({ b: 2 })});
 
     // will no exit master process util this worker is end. You can invoke process.exit() in the worker to make master not to be blocking.
 } else if(cluster.isWorker) {
-    console.log(`I am worker #${cluster.worker.id}, wid is: ${process.env.wid}`);
+    console.log(`I am worker #${cluster.worker.id}, wid is: ${process.env.preload}`);
+    console.log(JSON.parse(process.env.preload));
     // make worker to be end and the master will not be blocked there.
     process.exit();
 }
